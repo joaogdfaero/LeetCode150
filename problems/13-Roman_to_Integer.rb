bb329ee6baba5b4204635b2d315c327ecb8cf197
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-06-30 22:37:52 -0300

    feat: roman to integer

--- problems/13-Roman_to_Integer.rb
+++ problems/13-Roman_to_Integer.rb
@@ -62,3 +62,37 @@ end
 
 s = "IV"
 p roman_to_int(s)
+
+
+###############################################
+############## Better solution ################
+
+def roman_to_int2(s)
+    hash = {
+        'I'=> 1,
+        'V'=> 5,
+        'X'=> 10,
+        'L'=> 50,
+        'C'=> 100,
+        'D'=> 500,
+        'M'=> 1000
+    }
+
+    total = 0
+    i = 0
+
+    while i < s.length
+        if i + 1 < s.length && hash[s[i]] < hash[s[i+1]]
+            total += hash[s[i+1]] - hash[s[i]]
+            i += 2
+        else
+            total += hash[s[i]]
+            i += 2
+        end 
+    end
+
+    total
+end
+
+s = "IV"
+p roman_to_int2(s)
