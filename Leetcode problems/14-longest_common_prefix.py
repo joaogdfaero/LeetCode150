88374a2ab2aefd1d2941a19d5796cd79671ee625
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-12-18 08:46:24 -0300

    14_longest_common_prefix final version

--- Leetcode problems/14-longest_common_prefix.py
+++ Leetcode problems/14-longest_common_prefix.py
@@ -1,36 +1,28 @@
 def longest_common_prefix(strs):
-    if len(strs) == 1:
-        return strs[0]
-    
+    if not strs:
+        return ""
+
+    # Sort the strings to ensure that the common prefix is at the beginning
+    # In the context of finding the longest common prefix, sorting the list is useful
+    # because it ensures that the strings with the most similar prefixes are adjacent
+    # to each other. This simplifies the process of finding the common prefix by
+    # comparing only the first and last strings in the sorted list.
+
     strs.sort()
 
+    # Take the first and last strings after sorting
     first_str = strs[0]
-    second_str = strs[1]
-    common_prefix = ""
-    all_match = True 
+    last_str = strs[-1]
 
+    # Find the common prefix between the first and last strings
+    common_prefix = ""
     for i in range(len(first_str)):
-        if first_str[i] == second_str[i]:
+        if i < len(last_str) and first_str[i] == last_str[i]:
             common_prefix += first_str[i]
         else:
             break
 
-    if len(strs) > 2:
-        for i in range(2, len(strs)):
-            if not strs[i].startswith(common_prefix):
-                all_match = False
-
-    if not all_match:
-        return ""
-    else:
-        return common_prefix
-        
-        
-    # loop em cada elemento do array
-    # vê o que há em comum entre o primeiro e segundo
-    # vê se os outros objetos do array possuem isso (caso eles existam)
-    # retorna esse prefixo 
-
-
-strs = ["flower","flow","flight"]
+    return common_prefix
+         
+strs = ["abab","abbaa","cbc"]
 print(longest_common_prefix(strs))
