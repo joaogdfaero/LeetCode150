34acdd076d2891c8a53b1dde9b3e731f4a592bfc
João Gabriel Dal Forno <103076610+joaogdfaero@users.noreply.github.com>
2024-01-08 19:28:19 -0300

    1.Two sum

--- Leetcode problems/1-twosum.rb
+++ Leetcode problems/1-twosum.rb
@@ -1,21 +1,6 @@
-# https://leetcode.com/problems/two-sum/
-
-class Leetcode150
-    def two_sum(nums, target)
-        hash = {}
-
-        nums.each_with_index do |number, index|
-            if hash[target - number]
-                return [hash[target - number], index]
-            else
-                hash[number] = index
-            end 
-        end     
-    end
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+    # refazer
 end
-
-nums = [2,3,4,7] 
-target = 6
-
-test = Leetcode150.new
-p test.two_sum(nums, target)
