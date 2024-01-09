c4da86372f894e1f4e8789c39f4f8acc0b1aef3b
João Gabriel Dal Forno <103076610+joaogdfaero@users.noreply.github.com>
2024-01-09 07:28:49 -0300

    1. Two Sum

--- Leetcode problems/1-twosum.rb
+++ Leetcode problems/1-twosum.rb
@@ -2,5 +2,15 @@
 # @param {Integer} target
 # @return {Integer[]}
 def two_sum(nums, target)
-    # refazer
+    return [0, 1] if nums.length == 2 && nums.sum == target
+    missing_value = 0
+
+    nums.each_with_index do |num, index|
+        # calcula o valor que falta para completar o target
+        missing_value = target - num
+        # vê se esse valor existe em nums
+        found_match = nums.index(missing_value)
+        # se existir, retorna a posição dele e a posição do número atual
+        return [found_match, index] if found_match && found_match != index
+    end
 end
